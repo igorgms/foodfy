f46a406b2b82bcde288065e8391e950efdb65951
Igor Gomes <igorgmsf@gmail.com>
2020-07-29 22:15:34 -0300

    desafio foodfy finalizado

---
+++ script.js
@@ -0,0 +1,20 @@
+const modalOverlay = document.querySelector('.modal-overlay')
+const cards = document.querySelectorAll('.card')
+
+for(let card of cards){
+    card.addEventListener('click', function(){
+        const imgId = card.getAttribute('id')
+        const title = card.querySelector('h4').innerHTML
+        const auhtor = card.querySelector('p').innerHTML
+
+        modalOverlay.classList.add('active')
+
+        modalOverlay.querySelector('img').src =`./layouts/assets/${imgId}.png`
+        modalOverlay.querySelector('h4').innerText = `${title}`
+        modalOverlay.querySelector('p').innerHTML = `${auhtor}`
+    })
+}
+
+document.querySelector('.close-modal').addEventListener('click', function(){
+    modalOverlay.classList.remove('active')
+})
